e857fa4ec95da21a48aa5e1581d01cc411c49dd7
Rafaela Nobre <raafanobre@gmail.com>
2025-10-20 15:25:08 -0300

    Feat: Add unit tests for health check and home page

--- processor/tests.py
+++ processor/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class HealthCheckTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_health_check_returns_200(self):
+        response = self.client.get(reverse('health'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_health_check_returns_json(self):
+        response = self.client.get(reverse('health'))
+        self.assertEqual(response['Content-Type'], 'application/json')
+
+    def test_health_check_status_ok(self):
+        response = self.client.get(reverse('health'))
+        self.assertEqual(response.json(), {'status': 'OK'})
+
+class HomePageTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_page_returns_200(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'processor/home.html')
